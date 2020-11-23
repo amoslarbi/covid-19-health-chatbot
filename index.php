9efdd4fb67adf7d86953b40513a8f65739fe0d9c
Khay <larbiamos18@gmail.com>
2020-11-23 23:22:44 +0000

    Bug Fixes

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>COVID-19 Health Chat Bot</title>
+    <link rel="shortcut icon" type="image/x-icon" href="https://specials-images.forbesimg.com/imageserve/1211364644/960x0.jpg">
 </head>
 
 <body>
@@ -13,10 +14,10 @@
     <div class='rid_4f002aee_403277' data-apiroot='//tools.cdc.gov/api' data-mediatype='html' data-mediaid='403277' data-stripscripts='true' data-stripanchors='false' data-stripimages='false' data-stripcomments='true' data-stripstyles='true' data-cssclasses='syndicate,!no-syndicate' data-ids='' data-xpath='' data-oe='utf-8' data-of='xhtml' data-ns='cdc' data-postprocess='' data-nw='true' data-iframe='true' data-cdc-widget='syndicationIframe' data-apiembedsrc='//tools.cdc.gov/api/embed/html/js/embed-2.0.3.js' data-iframeembedsrc='//tools.cdc.gov/TemplatePackage/contrib/widgets/tp-widget-external-loader.js'></div>
     <script src='//tools.cdc.gov/TemplatePackage/contrib/widgets/tp-widget-external-loader.js'></script><noscript>You need javascript enabled to view this content or go to <a href='//tools.cdc.gov/api/v2/resources/media/403277/noscript'>source URL</a>.</noscript>
 
-    <!-- COVID chat bot start -->
+    <!-- covid-19-health-chatbot start -->
     <div data-cdc-widget='healthBot' data-cdc-theme='theme1' class='cdc-widget-color-white'  data-cdc-language='en-us'></div>
     <script src='https://t.cdc.gov/1M1B'></script>
-    <!-- COVID chat bot end -->
+    <!-- covid-19-health-chatbot bot end -->
 
 </body>
 
